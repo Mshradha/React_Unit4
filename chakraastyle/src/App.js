378b9b3d429b3f2bbf3f246ea3b83592de8bd07c
Mshradha <99673738+Mshradha@users.noreply.github.com>
2022-06-02 00:07:38 +0530

    x

---
+++ chakraastyle/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './ques2/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
